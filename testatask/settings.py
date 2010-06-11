f7c4d644274b0e38a9394a6baf22b17779ec880f
Michael <michael@creativecult.org>
2010-06-12 02:51:24 +0300

    basic views for groups (list/detail)

--- testatask/settings.py
+++ testatask/settings.py
@@ -1,5 +1,7 @@
 # Django settings for testatask project.
 
+from os import path
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -81,6 +83,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    path.abspath('tpl'),
 )
 
 INSTALLED_APPS = (
